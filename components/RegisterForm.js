c6c7fa689043b0b9754434895afbad4443a37a4b
MatanSultan <88273616+MatanSultan@users.noreply.github.com>
2023-04-11 11:52:32 +0300

    init

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -55,76 +55,94 @@ const RegisterForm = () => {
 
   return (
     <div className="min-h-screen bg-gray-100">
-      <form className=" max-w-md mx-auto " onSubmit={handleSubmit}>
-        <h2 className="text-2xl font-bold mb-4">Create an account</h2>
-        <div className="mb-4">
-          <label
-            htmlFor="username"
-            className="block text-gray-700 font-bold mb-2"
-          >
-            Username
-          </label>
-          <input
-            id="username"
-            type="text"
-            className="w-full border border-gray-400 p-2 rounded-md focus:outline-none focus:border-blue-500"
-            required
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
+      <div className="flex flex-col justify-center sm:py-12">
+        <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+          <div className="absolute inset-0 bg-gradient-to-r from-blue-500 to-indigo-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl" />
+          <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+            <form className=" max-w-md mx-auto " onSubmit={handleSubmit}>
+              <h2 className="text-2xl font-bold mb-4">Create an account</h2>
+              <div className="mb-4">
+                <label
+                  htmlFor="username"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Username
+                </label>
+                <input
+                  id="username"
+                  type="text"
+                  className="w-full border border-gray-400 p-2 rounded-md focus:outline-none focus:border-blue-500"
+                  required
+                  value={username}
+                  onChange={(event) => setUsername(event.target.value)}
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="email"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Email
+                </label>
+                <input
+                  id="email"
+                  type="email"
+                  className="w-full border border-gray-400 p-2 rounded-md focus:outline-none focus:border-blue-500"
+                  required
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="password"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Password
+                </label>
+                <input
+                  id="password"
+                  type="password"
+                  className="w-full border border-gray-400 p-2 rounded-md focus:outline-none focus:border-blue-500"
+                  required
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="confirmPassword"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Confirm Password
+                </label>
+                <input
+                  id="confirmPassword"
+                  type="password"
+                  className="w-full border border-gray-400 p-2 rounded-md focus:outline-none focus:border-blue-500"
+                  required
+                  value={confirmPassword}
+                  onChange={(event) => setConfirmPassword(event.target.value)}
+                />
+              </div>
+              <button
+                type="submit"
+                className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+              >
+                Register
+              </button>
+              <p className="mt-4 text-center">
+                Already have an account?{" "}
+                <Link href="/login">
+                  <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
+                    Login
+                  </button>
+                </Link>
+              </p>
+            </form>
+          </div>
         </div>
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
-            Email
-          </label>
-          <input
-            id="email"
-            type="email"
-            className="w-full border border-gray-400 p-2 rounded-md focus:outline-none focus:border-blue-500"
-            required
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="password"
-            className="block text-gray-700 font-bold mb-2"
-          >
-            Password
-          </label>
-          <input
-            id="password"
-            type="password"
-            className="w-full border border-gray-400 p-2 rounded-md focus:outline-none focus:border-blue-500"
-            required
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="confirmPassword"
-            className="block text-gray-700 font-bold mb-2"
-          >
-            Confirm Password
-          </label>
-          <input
-            id="confirmPassword"
-            type="password"
-            className="w-full border border-gray-400 p-2 rounded-md focus:outline-none focus:border-blue-500"
-            required
-            value={confirmPassword}
-            onChange={(event) => setConfirmPassword(event.target.value)}
-          />
-        </div>
-        <button
-          type="submit"
-          className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-        >
-          Register
-        </button>
-      </form>
+      </div>
     </div>
   );
 };

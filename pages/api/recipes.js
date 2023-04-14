6d79a01856e70eac4cda01cfb1fb04dcada59e8c
MatanSultan <88273616+MatanSultan@users.noreply.github.com>
2023-04-14 15:47:14 +0300

    add db key to vercel

--- pages/api/recipes.js
+++ pages/api/recipes.js
@@ -1,10 +1,8 @@
 import { con } from "../../lib/db";
 
 export default function handler(req, res) {
-  const sql = "SELECT * FROM recipes";
-
-  con.query(sql, function (error, results) {
-    if (error) throw error;
-    res.status(200).json(results);
+  con.query("SELECT * FROM recipes", function (err, result, fields) {
+    if (err) throw err;
+    res.status(200).json(result);
   });
 }
